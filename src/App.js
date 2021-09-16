96effed3708f38a0eb3a5790687f60dde5eb674a
agnesrausz <rauszka94@gmail.com>
2021-09-17 01:13:08 +0200

    Add home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import PokemonList from "./components/PokemonList";
 import TypeList from "./components/TypeList";
 import PokemonDetail from "./components/PokemonDetail";
+import Home from "./components/Home";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
             <div className="App">
                 <Navbar/>
                 <Switch>
-                    <Route path='/' exact/>
+                    <Route path='/' exact component={Home}/>
                     <Route path="/pokemons" exact component={PokemonList}/>
                     <Route path="/types" component={TypeList}/>
                     <Route path="/pokemon/:id" component={PokemonDetail}/>
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+
+    return (
+        <div>
+            <img className="img-home" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1920px-International_Pok%C3%A9mon_logo.svg.png" alt="pokemon"/>
+        </div>
+    );
+}
+
+export default Home;
