0be35934e20afc073a1530c3c92d3d56065fbd46
agnesrausz <rauszka94@gmail.com>
2021-09-17 01:14:33 +0200

    Implement load next elements

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -11,27 +11,43 @@ function PokemonList() {
     const [pokemons, setPokemons] = useState([]);
     const [pictures, setPictures] = useState([]);
     const [types, setTypes] = useState([]);
+    const [load, setLoad] = useState(0);
+    const [autoLoad, setAutoLoad] = useState(0);
+    const [loadMode, setLoadMode] = useState(load);
+
+    const [next, setNext] = useState("https://pokeapi.co/api/v2/pokemon");
+
 
     useEffect(() => {
         fetchPokemons();
+    }, [load])
 
-    }, [])
 
     const fetchPokemons = async () => {
-        const pokes = await axios.get("https://pokeapi.co/api/v2/pokemon")
-        let pokeList = pokes.data.results;
+        if (next) {
+            const pokes = await axios.get(next)
+            let pokeList = pokes.data.results;
 
-        setPokemons(pokeList);
+            console.log(pokes)
+            setNext(pokes.data.next)
+            if (pokemons.length === 0) {
+                setPokemons(pokeList);
+            }
 
-        for (let i = 0; i < pokeList.length; i++) {
-            await fetchPokemon(pokeList[i].url);
+            for (let i = 0; i < pokeList.length; i++) {
+                await fetchPokemon(pokeList[i].url);
+                if (pokemons.length !== 0) {
+                    setPokemons(state => [...state, pokeList[i]])
+                }
+            }
+            setAutoLoad(autoLoad + 1)
         }
     }
 
 
     const fetchPokemon = async (url) => {
         const poke = await axios.get(url);
-        console.log(url)
+
         let image = poke.data.sprites.other["official-artwork"].front_default;
         let type = poke.data.types[0].type.name;
 
@@ -42,6 +58,7 @@ function PokemonList() {
 
 
     return (
+        <div>
         <ul className='pokemon-list'>
             {pokemons.map((pokemon, index) => (
                 <PokemonCard
@@ -52,6 +69,9 @@ function PokemonList() {
                 />
             ))}
         </ul>
+            <div className='button-load' onClick={() => setLoad(load +1)}>Load more</div>
+            {/*<button onClick={() => setLoadMode(autoLoad)}>Load all</button>*/}
+        </div>
     );
 }
 
